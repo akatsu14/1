718f0903aeaf9e8f6439d83da1b70229e39ca8ef
akatsu14 <luong13166@gmail.com>
2023-10-11 22:15:18 +0700

    OOP

--- J04009/J04009.java
+++ J04009/J04009.java
@@ -9,8 +9,8 @@ class Point {
     private double x, y;
 
     Point() {
-        x = 0;
-        y = 0;
+        x = 1;
+        y = 1;
     }
 
     Point(double x, double y) {
@@ -32,7 +32,8 @@ class Point {
     }
 
     public Double distance(Point secondPoint) {
-        return Math.sqrt(Math.pow(Math.abs(this.x - secondPoint.x), 2) + Math.pow(Math.abs(this.y - secondPoint.y), 2));
+        return Math.sqrt(Math.pow(Math.abs(this.x - secondPoint.x), 2) +
+                Math.pow(Math.abs(this.y - secondPoint.y), 2));
     }
 
     @Override
@@ -40,24 +41,87 @@ class Point {
         return x + y + "";
     }
 }
+// class Point {
+// private double x, y;
+
+// Point() {
+// x = 1;
+// y = 1;
+// }
+
+// Point(Double x, Double y) {
+// this.x = x;
+// this.y = y;
+// }
+
+// double getX() {
+// return x;
+// }
+
+// double getY() {
+// return y;
+// }
+
+// double distance(Point secondPoint) {
+// return Math.sqrt(Math.pow(this.x - secondPoint.x, 2) + Math.pow(this.y -
+// secondPoint.y, 2));
+// }
+
+// double distance(Point p1, Point p2) {
+// return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+// }
+
+// public String toString() {
+// return x + " " + y;
+// }
+// }
 
 public class J04009 {
+    // public static void main(String[] args) {
+    // Scanner sc = new Scanner(System.in);
+    // int test = sc.nextInt();
+    // while (test-- > 0) {
+    // Point x = new Point(sc.nextDouble(), sc.nextDouble());
+    // Point y = new Point(sc.nextDouble(), sc.nextDouble());
+    // Point z = new Point(sc.nextDouble(), sc.nextDouble());
+    // Double a = x.distance(y);
+    // Double b = y.distance(z);
+    // Double c = x.distance(z);
+    // if (a + b > c && a + c > b && b + c > a && Math.abs(a - c) < b && Math.abs(a
+    // - b) < c
+    // && Math.abs(b - c) < a) {
+    // System.out.printf("%.2f\n", Math.sqrt((a + b + c) * (a + b - c) * (a - b + c)
+    // * (c - a + b)) / 4);
+    // } else
+    // System.out.println("INVALID");
+    // }
+    // sc.close();
+    // }
+    // (a + b + c) * (a + b - c) * (b + (c - a)) * (c + a - b)//sai
+    // (a + b + c) * (a + b - c) * (a - b + c) * (c - a + b)//dung
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int test = sc.nextInt();
-        while (test-- > 0) {
-            Point x = new Point(sc.nextDouble(), sc.nextDouble());
-            Point y = new Point(sc.nextDouble(), sc.nextDouble());
-            Point z = new Point(sc.nextDouble(), sc.nextDouble());
-            Double a = x.distance(y);
-            Double b = y.distance(z);
-            Double c = x.distance(z);
-            if (a + b > c && a + c > b && b + c > a && Math.abs(a - c) < b && Math.abs(a - b) < c
-                    && Math.abs(b - c) < a) {
-                System.out.printf("%.2f\n", Math.sqrt((a + b + c) * (a + b - c) * (a - b + c) * (c - a + b)) / 4);
-            } else
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            double x = sc.nextDouble();
+            double y = sc.nextDouble();
+            Point p1 = new Point(x, y);
+            x = sc.nextDouble();
+            y = sc.nextDouble();
+            Point p2 = new Point(x, y);
+            x = sc.nextDouble();
+            y = sc.nextDouble();
+            Point p3 = new Point(x, y);
+            double a = p1.distance(p2);
+            double b = p2.distance(p3);
+            double c = p3.distance(p1);
+            if (a == 0 || b == 0 || c == 0 || a + b <= c || b + c <= a || a + c <= b) {
                 System.out.println("INVALID");
+            } else {
+                double s = Math.sqrt((a + b + c) * (b - c + a) * (b + (c - a)) * (c + (a - b))) * 0.25;
+                System.out.printf("%.2f \n", s);
+            }
         }
-        sc.close();
     }
+
 }
